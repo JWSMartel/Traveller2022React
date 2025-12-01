361064336867256014d08f3d9b34f71c552e0bda
JWSMartel <JWSMartel@gmail.com>
2025-12-01 12:04:33 -0500

    Making btn reactive.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function App() {
+  const [planetDesc, setPlanetDesc] = useState("Planet Desc");
+
+  function PlanetBtnClicked(){
+    setPlanetDesc("Planet Btn Clicked!");
+  }
+
+  return (
+    <div>
+      <button className="PlanetBtn" onClick={PlanetBtnClicked}>Create a Planet</button>
+      <p>{planetDesc}</p>
+    </div>
+  );
 }
